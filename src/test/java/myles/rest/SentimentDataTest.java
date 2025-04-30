02444ed33402c305812949178f22a5e6a6564842
myless <santapagamyles@gmail.com>
2025-04-29 21:16:50 -0400

    adding tests

---
+++ src/test/java/myles/rest/SentimentDataTest.java
@@ -0,0 +1,23 @@
+package myles.rest;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SentimentDataTest {
+
+    @Test
+    void combinePhrasesWithSentiment() {
+        assertAll(
+                () -> assertEquals()
+        );
+    }
+
+    @Test
+    void organizeData() {
+    }
+
+    @Test
+    void txtFileToArray() {
+    }
+}
